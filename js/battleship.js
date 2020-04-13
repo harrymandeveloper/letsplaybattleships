73ed9b002c6dac23afb2bfeb670318340d57b086
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-04-13 13:22:18 +0100

    Testing code added to battleship.js file view responds to testing okay

--- js/battleship.js
+++ js/battleship.js
@@ -1,20 +1,31 @@
 let view = {
+
   displayMessage: function(msg){
-    // code to go in here
+    let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
   },
 
   displayHit: function(location) {
-    // code to go in here
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
 
   displayMiss: function(location) {
-    // code to go in here
-
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
   },
 
 };
 
 
+view.displayMiss("00"); 
+view.displayHit("34"); 
+view.displayMiss("55"); 
+view.displayHit("12"); 
+view.displayMiss("25"); 
+view.displayHit("26");
+
+view.displayMessage("This is a test of the hit or miss alert system");
 
 // ====
 
