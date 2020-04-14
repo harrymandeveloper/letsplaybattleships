f97ab0995b33ab41649ab4e9450570966d7a46ad
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-04-14 11:09:49 +0100

    Model assembly complete and display messages for hit miss and sink added

--- js/battleship.js
+++ js/battleship.js
@@ -1,45 +1,84 @@
 let view = {
-
-  displayMessage: function(msg){
+  displayMessage: function (msg) {
     let messageArea = document.getElementById("messageArea");
-        messageArea.innerHTML = msg;
+    messageArea.innerHTML = msg;
   },
 
-  displayHit: function(location) {
+  displayHit: function (location) {
     let cell = document.getElementById(location);
     cell.setAttribute("class", "hit");
   },
 
-  displayMiss: function(location) {
+  displayMiss: function (location) {
     let cell = document.getElementById(location);
     cell.setAttribute("class", "miss");
   },
-
 };
 
-
-// view.displayMiss("00"); 
-// view.displayHit("34"); 
-// view.displayMiss("55"); 
-// view.displayHit("12"); 
-// view.displayMiss("25"); 
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
 // view.displayHit("26");
 
 // view.displayMessage("This is a test of the hit or miss alert system");
 
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
 
-let ships = [
-  {
-    locations: ["10", "20", "30"],
-    hits: ["", "", ""]
-  },
-  {
-    locations: ["32", "33", "34"],
-    hits: ["", "", ""]
-  },
-  {
-    locations: ["63", "64", "65"],
-    hits: ["", "", "hit"]
-  }
+  ships: [
+    {
+      // Equivalent to A6, B6 and C6
+      locations: ["06", "16", "26"],
+      hits: ["", "", ""],
+    },
+    {
+      // Equivalent to D2, D3 and D4
+      locations: ["32", "33", "34"],
+      hits: ["", "", ""],
+    },
+    {
+      // Equivalent to G3, G4 and G5
+      locations: ["63", "64", "65"],
+      hits: ["", "", ""],
+    },
+  ],
 
-]
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i+=1) {
+      let ships = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+      // So if the indexOf here returns -1 then there are no matches.
+      
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess); 
+        view.displayMessage("HIT!");
+
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank one of my cruisers!");
+          this.shipsSunk+=1;
+        }
+        return true;
+
+      }
+
+    }
+    view.displayMiss(guess); 
+    view.displayMessage("You missed.");  
+    return false;
+  },
+
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
+};
